f25dff3211a526b2619f16c1f7c879ea77523594
Kai Davenport <kaiyadavenport@gmail.com>
2013-10-08 22:06:54 +0000

    added event emitters for oauth server

--- src/index.js
+++ src/index.js
@@ -17,6 +17,7 @@
 */
 
 var authom = require('authom');
+var OAuth2Provider = require('oauth2-provider').OAuth2Provider
 var express = require('express');
 var _ = require('lodash');
 
@@ -25,6 +26,7 @@ module.exports = function(options){
 	options = options || {};
 	
 	var route = options.id || '/auth';
+	
 
 	var paths = _.defaults(options.paths || {}, {
 		status:'/status',
@@ -36,6 +38,11 @@ module.exports = function(options){
 		post_login:'/',
 		post_register:'/'
 	})
+
+	// oauth server
+	var crypt_key = options.crypt_key || 'crypt_key';
+	var sign_key = options.sign_key || 'sign_key';
+	var myOAP = new OAuth2Provider({crypt_key: crypt_key, sign_key: sign_key});
 
 	var providers = options.providers || {};
 
@@ -214,6 +221,108 @@ module.exports = function(options){
 		var service = authom.createServer(options);
 		services[name] = service;
 	})
+
+
+
+
+	// before showing authorization page, make sure the user is logged in
+	myOAP.on('enforce_login', function(req, res, authorize_url, next) {
+	  if(req.session.user) {
+	    next(req.session.user);
+	  } else {
+	    res.writeHead(303, {Location: '/login?next=' + encodeURIComponent(authorize_url)});
+	    res.end();
+	  }
+	});
+
+	// render the authorize form with the submission URL
+	// use two submit buttons named "allow" and "deny" for the user's choice
+	myOAP.on('authorize_form', function(req, res, client_id, authorize_url) {
+	  //res.end('<html>this app wants to access your account... <form method="post" action="' + authorize_url + '"><button name="allow">Allow</button><button name="deny">Deny</button></form>');
+	  app.emit('authorize_form', req, res, client_id, authorize_url);
+	  
+	});
+
+	// save the generated grant code for the current user
+	myOAP.on('save_grant', function(req, client_id, code, next) {
+		app.emit('save_grant', req, client_id, code, next);
+	})
+
+	/*
+	  if(!(req.session.user in myGrants))
+	    myGrants[req.session.user] = {};
+
+	  myGrants[req.session.user][client_id] = code;
+	  next();
+	});*/
+
+	// remove the grant when the access token has been sent
+	myOAP.on('remove_grant', function(user_id, client_id, code) {
+		app.emit('remove_grant', user_id, client_id, code);
+	  //if(myGrants[user_id] && myGrants[user_id][client_id])
+	  //  delete myGrants[user_id][client_id];
+	});
+
+	// find the user for a particular grant
+	myOAP.on('lookup_grant', function(client_id, client_secret, code, next) {
+	  // verify that client id/secret pair are valid
+
+	  app.emit('lookup_grant', client_id, client_secret, code, next);
+	  /*
+	  if(client_id in myClients && myClients[client_id] == client_secret) {
+	    for(var user in myGrants) {
+	      var clients = myGrants[user];
+
+	      if(clients[client_id] && clients[client_id] == code)
+	        return next(null, user);
+	    }
+	  }
+
+	  next(new Error('no such grant found'));
+	  */
+	});
+
+	// embed an opaque value in the generated access token
+	myOAP.on('create_access_token', function(user_id, client_id, next) {
+		app.emit('create_access_token', user_id, client_id, next);
+	  //var extra_data = 'blah'; // can be any data type or null
+	  //var oauth_params = {token_type: 'bearer'};
+
+	  //next(extra_data/*, oauth_params*/);
+	});
+
+	// (optional) do something with the generated access token
+	myOAP.on('save_access_token', function(user_id, client_id, access_token) {
+	  //console.log('saving access token %s for user_id=%s client_id=%s', JSON.stringify(access_token), user_id, client_id);
+	  app.emit('save_access_token', user_id, client_id, access_token);
+	});
+
+	// an access token was received in a URL query string parameter or HTTP header
+	myOAP.on('access_token', function(req, token, next) {
+	  var TOKEN_TTL = 10 * 60 * 1000; // 10 minutes
+
+	  if(token.grant_date.getTime() + TOKEN_TTL > Date.now()) {
+	    req.session.user = token.user_id;
+	    req.session.data = token.extra_data;
+	  } else {
+	    console.warn('access token for user %s has expired', token.user_id);
+	  }
+
+	  next();
+	});
+
+	// (optional) client authentication (xAuth) for trusted clients
+	/*
+	myOAP.on('client_auth', function(client_id, client_secret, username, password, next) {
+	  if(client_id == '1' && username == 'guest') {
+	    var user_id = '1337';
+
+	    return next(null, user_id);
+	  }
+
+	  return next(new Error('client authentication denied'));
+	});
+	*/
 
 
 	/*
