f35cd74f8ce094e2aa14d135a05ccf56e8b850c7
Kai Davenport <kaiyadavenport@gmail.com>
2014-01-26 14:36:56 +0000

    remove ouath provider

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@
 */
 
 var authom = require('authom');
-var OAuth2Provider = require('oauth2-provider').OAuth2Provider
+//var OAuth2Provider = require('oauth2-provider').OAuth2Provider
 var express = require('express');
 var _ = require('lodash');
 
@@ -39,9 +39,9 @@ module.exports = function(options){
 	})
 
 	// oauth server
-	var crypt_key = options.crypt_key || 'crypt_key';
-	var sign_key = options.sign_key || 'sign_key';
-	var myOAP = new OAuth2Provider({crypt_key: crypt_key, sign_key: sign_key});
+	//var crypt_key = options.crypt_key || 'crypt_key';
+	//var sign_key = options.sign_key || 'sign_key';
+	//var myOAP = new OAuth2Provider({crypt_key: crypt_key, sign_key: sign_key});
 
 	var providers = options.providers || {};
 
@@ -230,10 +230,13 @@ module.exports = function(options){
 		
 	})
 
+	app.get(route + '/:service', authom.app);
+
 
 
 
 	// before showing authorization page, make sure the user is logged in
+	/*
 	myOAP.on('enforce_login', function(req, res, authorize_url, next) {
 	  if(req.session.user) {
 	    next(req.session.user);
@@ -241,20 +244,22 @@ module.exports = function(options){
 	    res.writeHead(303, {Location: paths.login + '?next=' + encodeURIComponent(authorize_url)});
 	    res.end();
 	  }
-	});
+	});*/
 
 	// render the authorize form with the submission URL
 	// use two submit buttons named "allow" and "deny" for the user's choice
+	/*
 	myOAP.on('authorize_form', function(req, res, client_id, authorize_url) {
 	  //res.end('<html>this app wants to access your account... <form method="post" action="' + authorize_url + '"><button name="allow">Allow</button><button name="deny">Deny</button></form>');
 	  app.emit('authorize_form', req, res, client_id, authorize_url);
 	  
-	});
+	});*/
 
 	// save the generated grant code for the current user
+	/*
 	myOAP.on('save_grant', function(req, client_id, code, next) {
 		app.emit('save_grant', req, client_id, code, next);
-	})
+	})*/
 
 	/*
 	  if(!(req.session.user in myGrants))
@@ -265,18 +270,20 @@ module.exports = function(options){
 	});*/
 
 	// remove the grant when the access token has been sent
+	/*
 	myOAP.on('remove_grant', function(user_id, client_id, code) {
 		app.emit('remove_grant', user_id, client_id, code);
 	  //if(myGrants[user_id] && myGrants[user_id][client_id])
 	  //  delete myGrants[user_id][client_id];
-	});
+	});*/
 
 	// find the user for a particular grant
+	/*
 	myOAP.on('lookup_grant', function(client_id, client_secret, code, next) {
 	  // verify that client id/secret pair are valid
 
 	  app.emit('lookup_grant', client_id, client_secret, code, next);
-	  /*
+	  
 	  if(client_id in myClients && myClients[client_id] == client_secret) {
 	    for(var user in myGrants) {
 	      var clients = myGrants[user];
@@ -287,25 +294,28 @@ module.exports = function(options){
 	  }
 
 	  next(new Error('no such grant found'));
-	  */
-	});
+	  
+	});*/
 
 	// embed an opaque value in the generated access token
+	/*
 	myOAP.on('create_access_token', function(user_id, client_id, next) {
 		app.emit('create_access_token', user_id, client_id, next);
 	  //var extra_data = 'blah'; // can be any data type or null
 	  //var oauth_params = {token_type: 'bearer'};
 
-	  //next(extra_data/*, oauth_params*/);
-	});
+	  
+	});*/
 
 	// (optional) do something with the generated access token
+	/*
 	myOAP.on('save_access_token', function(user_id, client_id, access_token) {
 	  //console.log('saving access token %s for user_id=%s client_id=%s', JSON.stringify(access_token), user_id, client_id);
 	  app.emit('save_access_token', user_id, client_id, access_token);
-	});
+	});*/
 
 	// an access token was received in a URL query string parameter or HTTP header
+	/*
 	myOAP.on('access_token', function(req, token, next) {
 	  var TOKEN_TTL = 10 * 60 * 1000; // 10 minutes
 
@@ -317,7 +327,7 @@ module.exports = function(options){
 	  }
 
 	  next();
-	});
+	});*/
 
 	// (optional) client authentication (xAuth) for trusted clients
 	/*
@@ -339,7 +349,7 @@ module.exports = function(options){
 		
 	*/
 
-	app.get(route + '/:service', authom.app);
+	
 
 	return app;
 }
